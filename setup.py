961b0844f2998acd9d5c1bd961dc3a2b226a7fcd
x22212311 <x22212311@student.ncirl.ie>
2024-03-23 20:14:33 +0000

    third commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    HYPEN_E_DOT='-e .'
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("/n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+setup(
+name='mlproject',
+version='0.0.1',
+author='Gaurav',
+author_email='x22212311@student.ncirl.ie',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
